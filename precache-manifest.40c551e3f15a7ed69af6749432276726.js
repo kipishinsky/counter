a8e121006b4030c17b1d82c8a4238ae1b406b003
SS007 <sergeysevostyanov007@gmail.com>
2020-04-22 16:50:35 +0400

    Updates

---
+++ precache-manifest.40c551e3f15a7ed69af6749432276726.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39aabbce3c926afdcc5ef15b08694a3a",
+    "url": "/counter/index.html"
+  },
+  {
+    "revision": "a7e9fb9a6004e4b6eeee",
+    "url": "/counter/static/css/main.2b285567.chunk.css"
+  },
+  {
+    "revision": "f0650d0bb2ce4374fad8",
+    "url": "/counter/static/js/2.dd4f2411.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/counter/static/js/2.dd4f2411.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7e9fb9a6004e4b6eeee",
+    "url": "/counter/static/js/main.bf70214c.chunk.js"
+  },
+  {
+    "revision": "345446230396b9bddf62",
+    "url": "/counter/static/js/runtime-main.37c0ace9.js"
+  }
+]);
